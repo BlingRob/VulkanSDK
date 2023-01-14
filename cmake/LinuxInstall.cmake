522647f9b13d3c5a778d24243f4278b717106d55
BlingRob <gisaku2010@rambler.ru>
2023-01-14 16:53:39 +0300

    Bugfix: didn't install env vars if project has being using as subproject

--- cmake/LinuxInstall.cmake
+++ cmake/LinuxInstall.cmake
@@ -3,12 +3,12 @@ set(VULKAN_SDK_LIBRARY_DIR ${CMAKE_INSTALL_PREFIX}/lib64 CACHE STRING "Vulkan li
 add_custom_command(
 TARGET ${PROJECT_NAME}
 PRE_BUILD
-COMMAND ${CMAKE_SOURCE_DIR}/scripts/setx.sh VK_LAYER_PATH ${CMAKE_INSTALL_PREFIX}/share/vulkan/explicit_layer.d)
+COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/scripts/setx.sh VK_LAYER_PATH ${CMAKE_INSTALL_PREFIX}/share/vulkan/explicit_layer.d)
 
 add_custom_command(
 TARGET ${PROJECT_NAME}
 PRE_BUILD
-COMMAND ${CMAKE_SOURCE_DIR}/scripts/setx.sh VK_LOADER_DEBUG "all")
+COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/scripts/setx.sh VK_LOADER_DEBUG "all")
 
 list(APPEND Vulkan_Libs "${CMAKE_INSTALL_PREFIX}/lib64/libglslang.a")
 list(APPEND Vulkan_Libs "${CMAKE_INSTALL_PREFIX}/lib64/libSPIRV.a")
